b9ab629c507226c362c0f295fd71d8ca5723d5bd
lucasbravi2019 <80082740+lucasbravi2019@users.noreply.github.com>
2023-02-01 21:43:04 -0300

    Initial template (#1) (#2)

---
+++ src/root/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './app.css'
+
+const App: React.FC = () => {
+  return (
+    <h1>React with Redux - Saga - TypeScript - Router</h1>
+  )
+}
+
+export default App
