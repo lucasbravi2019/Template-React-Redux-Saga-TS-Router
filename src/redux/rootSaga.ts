0775e086513b05029f31ce54830b3292309503ce
lucasbravi2019 <80082740+lucasbravi2019@users.noreply.github.com>
2023-02-01 21:41:53 -0300

    Initial template

--- src/redux/rootSaga.ts
+++ src/redux/rootSaga.ts
@@ -1,10 +1,9 @@
-import { all, fork, AllEffect, ForkEffect } from 'redux-saga/effects';
-import counterSagas from './counter/saga';
+import { AllEffect, ForkEffect } from 'redux-saga/effects';
 
 export default function* rootSaga(): Generator<
   AllEffect<ForkEffect<void>>,
   void,
   unknown
 > {
-  yield all([fork(counterSagas)]);
+  // include yield all([fork( yourRootSaga )]);
 }
